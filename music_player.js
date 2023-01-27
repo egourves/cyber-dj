2064ee3c68561f9e24e6da31a7b108c223a73559
egvs <egourves@enssat.fr>
2023-01-27 16:07:24 +0100

    iframe

--- music_player.js
+++ music_player.js
@@ -11,6 +11,12 @@ A shared music player:
 //define audio context
 const audioContext = new AudioContext();
 
+//index of the poisition in the playlist
+var i =0;
+
+//define var that contains all the music urls
+var playlist = [];
+
 // create event listeners on the play buttons
 document.getElementById('play').addEventListener('click', play);
 
@@ -32,6 +38,14 @@ document.getElementById('add_song').addEventListener('click', add_song);
 //get user input, put it in the playlist, and clear the input field
 function add_song(){
     var song = document.getElementById('url').value;
+    console.log(song);
     document.getElementById('playlist').innerHTML += song + '<br>';
+    playlist.append(song);
     document.getElementById('song').value = '';
+    console.log(playlist);
     }
+
+//play the song
+function play(){
+    player.innerHTML = "<iframe id=\"player\" type=\"text/html\" width=\"640\" height=\"390\"src = " + playlist[i] + "?enablejsapi=1&origin=https://egourves.github.io\" frameborder=\"0\"></iframe>";
+}
